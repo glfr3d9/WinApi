3e672cdff7168a9e238395f4d9f26d127546544b
glfr3d9 <99174559+glfr3d9@users.noreply.github.com>
2023-04-22 20:53:01 +0300

    Мьютексы

---
+++ lab2/ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <Windows.h>
+#include <cstring>
+
+struct SharedData {
+	int sch = 0;
+	char mas[8];
+};
+
+HANDLE mutex;
+HANDLE mapping;
+using namespace std;
+
+int main() {
+	DWORD dwBytesWritten = 0;
+	HANDLE hFile = CreateFile(TEXT("test1.txt"), GENERIC_WRITE | GENERIC_READ, FILE_SHARE_WRITE | FILE_SHARE_READ, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	mapping = CreateFileMappingA(hFile, nullptr, PAGE_READWRITE, 0, sizeof(SharedData), "first1");
+	unique_ptr<SharedData, decltype(&UnmapViewOfFile)> data((SharedData*)MapViewOfFile(mapping, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, 0), UnmapViewOfFile);
+	mutex = OpenMutexA(MUTEX_ALL_ACCESS, TRUE, "first");
+	if (mutex == NULL) {
+		mutex = CreateMutexA(NULL, FALSE, "first");
+	}
+	cout << "mutex:" << mutex << endl;
+	DWORD dwBytesToWrite = 13;
+	thread user_input_thread([&] {
+		WaitForSingleObject(mutex, INFINITE);
+		for (int i = 0; i < 4; i++) {
+			data->mas[data->sch] = '3';
+			cout << "mas[" << data->sch << "]= " << data->mas[data->sch] << " ";
+			WriteFile(hFile, (LPCVOID)data->mas[data->sch], dwBytesToWrite, &dwBytesWritten, NULL);
+			Sleep(500);
+			data->sch++;
+		}
+		ReleaseMutex(mutex);
+		});
+	user_input_thread.join();
+	system("pause");
+	CloseHandle(hFile);
+}
